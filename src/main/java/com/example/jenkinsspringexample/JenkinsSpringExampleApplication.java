988b681ebd5b10ecaa7dbc577c9ce0c999b2ad1e
Bartkowiak Szymon <szymon.bartkowiak@exorigo-upos.pl>
2025-10-14 16:02:59 +0200

    add

---
+++ src/main/java/com/example/jenkinsspringexample/JenkinsSpringExampleApplication.java
@@ -0,0 +1,19 @@
+package com.example.jenkinsspringexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class JenkinsSpringExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsSpringExampleApplication.class, args);
+    }
+
+
+    @GetMapping
+    public String get() {
+        return "Elo mordo lecimy se tu dalej";
+    }
+}
---
+++ src/test/java/com/example/jenkinsspringexample/JenkinsSpringExampleApplicationTests.java
@@ -0,0 +1,28 @@
+package com.example.jenkinsspringexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class JenkinsSpringExampleApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect((ResultMatcher) content().string("Elo mordo lecimy se tu dalej"));
+    }
+
+}
