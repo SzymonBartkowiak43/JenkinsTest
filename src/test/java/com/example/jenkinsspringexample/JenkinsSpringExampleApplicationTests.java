6adb377cc37dd3c9e0b56e18ec1f3bc6d08d02a0
Bartkowiak Szymon <szymon.bartkowiak@exorigo-upos.pl>
2025-10-15 10:31:05 +0200

    fix

--- src/test/java/com/example/jenkinsspringexample/JenkinsSpringExampleApplicationTests.java
+++ src/test/java/com/example/jenkinsspringexample/JenkinsSpringExampleApplicationTests.java
@@ -1,13 +1,14 @@
 package com.example.jenkinsspringexample;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+// Poprawiony import! Usunęliśmy błędny i dodaliśmy poprawny.
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -24,7 +25,6 @@ class JenkinsSpringExampleApplicationTests {
         this.mockMvc.perform(get("/"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect((ResultMatcher) content().string("Kebabb"));
+                .andExpect(content().string("Kebabbb"));
     }
-
 }
